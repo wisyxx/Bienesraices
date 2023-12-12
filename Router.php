baf4d687126b0da4c0a8ea29d009ffed19e17c4c
Álvaro Hernández Colino <alvarohcolino@gmail.com>
2023-12-12 19:06:41 +0100

    Start creating the router and the first controller, PropiedadController

--- Router.php
+++ Router.php
@@ -4,8 +4,25 @@ namespace MVC;
 
 class Router
 {
-    function __construct()
-    {   
-        echo 'Construyendo el router';
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn) {
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas()
+    {
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+        if ($metodo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if ($fn) { // La URL existe
+            call_user_func($fn, $this);
+        } else {
+            echo 'Page not found';
+        }
     }
 }
--- public/index.php
+++ public/index.php
@@ -3,5 +3,13 @@
 require_once __DIR__ . '../../includes/app.php';
 
 use MVC\Router;
+use Controllers\PropiedadController;
 
+$controller = new PropiedadController();
 $router = new Router();
+
+$router->get('/admin', [PropiedadController::class, 'index']);
+$router->get('/propiedades/crear', [PropiedadController::class, 'create']);
+$router->get('/propiedades/actualizar', [PropiedadController::class, 'update']);
+
+$router->comprobarRutas();
